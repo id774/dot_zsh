de90a0eaeff8a755775dd33ebb333b9d0936c84c
id774 <idnanashi@gmail.com>
2012-07-23 10:38:06 +0900

    32 Add env PROXY

--- dot_zsh/plugins/proxy.zsh
+++ dot_zsh/plugins/proxy.zsh
@@ -1,8 +1,9 @@
 # proxy.zsh
-# Last Change: 24-May-2011.
+# Last Change: 23-Jul-2012.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 #export http_proxy=http://proxy.hoge.co.jp:8080
 #export ftp_proxy=$http_proxy
 #export HTTP_PROXY=$http_proxy
 #export FTP_PROXY=$http_proxy
+#export PROXY=proxy.hoge.co.jp

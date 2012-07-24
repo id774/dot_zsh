53d481d41ea10a66082131580d5cb456e523f0d5
id774 <idnanashi@gmail.com>
2012-07-24 15:10:00 +0900

    36 Fix default hadoop home

--- dot_zsh/plugins/hadoop.zsh
+++ dot_zsh/plugins/hadoop.zsh
@@ -2,7 +2,7 @@
 # Last Change: 24-Jul-2012.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
-#export HADOOP_HOME=/etc/hadoop-0.20
+#export HADOOP_HOME=/usr
 #export HADOOP_ROOT=/usr/lib/hadoop
 #export HADOOP_JAR=$HADOOP_ROOT/contrib/streaming/hadoop-streaming-0.20.2-cdh3u4.jar
 #export HIVE_HOME=/opt/hive/0.9.0

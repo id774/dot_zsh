d57e451ae9a7eb8a54996c239aacc4f5400c076f
id774 <idnanashi@gmail.com>
2013-01-16 13:38:40 +0900

    40 Change error prompt color

--- dot_zsh/plugins/prompt.zsh
+++ dot_zsh/plugins/prompt.zsh
@@ -1,10 +1,10 @@
 # prompt.zsh
-# Last Change: 16-Dec-2011.
+# Last Change: 16-Jan-2013.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 if [ "$TERM" != "dumb" ]; then
-    PROMPT="%{$reset_color%}%{$fg[green]%}%(!.#.$) %{$reset_color%}"
+    PROMPT="%{$reset_color%}%{%(?.$fg[green].$fg[red])%}%(!.#.$) %{$reset_color%}"
     PROMPT="%{$reset_color%}%{$fg[green]%}:%{$fg[magenta]%}%B%~%b%{$reset_color%}$PROMPT"
     PROMPT="%{$reset_color%}%{$fg[yellow]%}%m%{$reset_color%}$PROMPT"
-    PROMPT="%{$reset_color%}%{%(?.$fg[cyan].$fg[red])%}%*%{$reset_color%} $PROMPT"
+    PROMPT="%{$reset_color%}%{$fg[cyan]%}%*%{$reset_color%} $PROMPT"
 fi

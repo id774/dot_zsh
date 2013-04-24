a78486eb7c47f31156fe024472f3b42e395fcb8f
id774 <idnanashi@gmail.com>
2013-04-24 16:02:11 +0900

    53 Append AWS variables

--- dot_zsh/plugins/amazonaws.zsh
+++ dot_zsh/plugins/amazonaws.zsh
@@ -1,5 +1,5 @@
 # amazonaws.zsh
-# Last Change: 23-Apr-2013.
+# Last Change: 24-Apr-2013.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 set_aws_path() {
@@ -12,7 +12,14 @@ set_aws_path() {
     done
 }
 
-export EC2_HOME=/opt/aws
+export AWS_PATH=/opt/aws
+export AWS_ELB_HOME=$AWS_PATH/apitools/elb
+export AWS_IAM_HOME=$AWS_PATH/apitools/iam
+export AWS_RDS_HOME=$AWS_PATH/apitools/rds
+export AWS_AUTO_SCALING_HOME=$AWS_PATH/apitools/as
+export AWS_CLOUDWATCH_HOME=$AWS_PATH/apitools/mon
+export EC2_HOME=$AWS_PATH/apitools/ec2
+export EC2_AMITOOL_HOME=$AWS_PATH/amitools/ec2
 # export EC2_PRIVATE_KEY=$HOME/.aws/pk-XXXXXXXX.pem
 # export EC2_CERT=$HOME/.aws/cert-XXXXXXXX.pem
 # export EC2_URL=http://ec2.ap-northeast-1.amazonaws.com
@@ -20,4 +27,12 @@ export EC2_HOME=/opt/aws
 # export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 set_aws_path \
-  $EC2_HOME
+  $AWS_PATH \
+  $AWS_ELB_HOME \
+  $AWS_IAM_HOME \
+  $AWS_RDS_HOME \
+  $AWS_AUTO_SCALING_HOME \
+  $AWS_CLOUDWATCH_HOME \
+  $EC2_HOME \
+  $EC2_AMITOOL_HOME
+

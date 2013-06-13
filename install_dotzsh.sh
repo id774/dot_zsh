f52c05a4eb99bec69e272b127793504319b92ab7
id774 <idnanashi@gmail.com>
2013-06-13 11:45:23 +0900

    59 Add cassandra plugin

--- install_dotzsh.sh
+++ install_dotzsh.sh
@@ -45,6 +45,7 @@ zsh_compile() {
     zsh -c 'zcompile $SOURCE/dot_zsh/lib/screen.zsh'
     zsh -c 'zcompile $SOURCE/dot_zsh/plugins/alias.zsh'
     zsh -c 'zcompile $SOURCE/dot_zsh/plugins/amazonaws.zsh'
+    zsh -c 'zcompile $SOURCE/dot_zsh/plugins/cassandra.zsh'
     zsh -c 'zcompile $SOURCE/dot_zsh/plugins/cryptfs.zsh'
     zsh -c 'zcompile $SOURCE/dot_zsh/plugins/ldlib.zsh'
     zsh -c 'zcompile $SOURCE/dot_zsh/plugins/prompt.zsh'
@@ -67,6 +68,7 @@ zwc_cleanup() {
     rm -f $SOURCE/dot_zsh/lib/screen.zsh.zwc
     rm -f $SOURCE/dot_zsh/plugins/alias.zsh.zwc
     rm -f $SOURCE/dot_zsh/plugins/amazonaws.zsh.zwc
+    rm -f $SOURCE/dot_zsh/plugins/cassandra.zsh.zwc
     rm -f $SOURCE/dot_zsh/plugins/cryptfs.zsh.zwc
     rm -f $SOURCE/dot_zsh/plugins/ldlib.zsh.zwc
     rm -f $SOURCE/dot_zsh/plugins/prompt.zsh.zwc

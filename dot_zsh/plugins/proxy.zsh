acfa1b2b586e28cd4133684aeae78616945d3ef9
id774 <idnanashi@gmail.com>
2013-07-24 16:13:40 +0900

    65 Set env no_proxy

--- dot_zsh/plugins/proxy.zsh
+++ dot_zsh/plugins/proxy.zsh
@@ -1,5 +1,5 @@
 # proxy.zsh
-# Last Change: 24-Jul-2012.
+# Last Change: 24-Jul-2013.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 #export PROXY=proxy.hoge.co.jp
@@ -9,3 +9,4 @@
 #export HTTP_PROXY=$http_proxy
 #export HTTPS_PROXY=$http_proxy
 #export FTP_PROXY=$http_proxy
+#export no_proxy="localhost,127.0.0.0/8,192.168.0.0/16,172.16.0.0/16,*.local"

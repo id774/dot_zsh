afcddaeb66db3a322a6e029fc8d3fa5159d20f4a
id774 <idnanashi@gmail.com>
2013-10-30 11:21:00 +0900

    68 Append LD_FLAGS

--- dot_zsh/plugins/ldlib.zsh
+++ dot_zsh/plugins/ldlib.zsh
@@ -1,5 +1,6 @@
 # ldlib.zsh
-# Last Change: 21-Dec-2011.
+# Last Change: 29-Oct-2013.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib
+export LD_FLAGS=/usr/local/lib:/usr/lib

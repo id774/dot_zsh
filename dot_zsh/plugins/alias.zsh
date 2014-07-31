5cfc024ab8dbd4b52623bd685defe8af8df1bd6a
id774 <idnanashi@gmail.com>
2014-07-31 11:40:04 +0900

    77 Suffix aliases

--- dot_zsh/plugins/alias.zsh
+++ dot_zsh/plugins/alias.zsh
@@ -1,6 +1,24 @@
 # alias.zsh
-# Last Change: 15-May-2014.
+# Last Change: 31-Jul-2014.
 # Maintainer:  id774 <idnanashi@gmail.com>
+
+function extract() {
+  case $1 in
+    *.tar.gz|*.tgz) tar xzvf $1;;
+    *.tar.xz) tar Jxvf $1;;
+    *.zip) unzip $1;;
+    *.lzh) lha e $1;;
+    *.tar.bz2|*.tbz) tar xjvf $1;;
+    *.tar.Z) tar zxvf $1;;
+    *.gz) gzip -d $1;;
+    *.bz2) bzip2 -dc $1;;
+    *.Z) uncompress $1;;
+    *.tar) tar xvf $1;;
+    *.arj) unarj $1;;
+  esac
+}
+
+function runcpp () { g++ $1 && shift && ./a.out $@ }
 
 set_alias() {
     alias pd='popd'
@@ -65,6 +83,12 @@ set_alias() {
     alias be='bundle exec'
     alias sqlite3='sqlite3 -header -csv -nullvalue "NULL"'
     alias emacs-compile='emacs --batch -Q -f batch-byte-compile'
+    alias -s pl=perl
+    alias -s py=python
+    alias -s rb=ruby
+    alias -s hs=runhaskell
+    alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
+    alias -s {c,cpp}=runcpp
 }
 
 set_alias

c436661b13a7a75719aa72d5ca75e8bb19ea1600
id774 <idnanashi@gmail.com>
2025-01-18 22:46:30 +0900

    135 Remove outdated plugins: amazonaws.zsh, cryptfs.zsh, and hadoop.zsh

--- dot_zsh/plugins/hadoop.zsh
+++
@@ -1,9 +0,0 @@
-# hadoop.zsh
-# Last Change: 12-Jun-2013.
-# Maintainer:  id774 <idnanashi@gmail.com>
-
-#export HADOOP_HOME=/usr
-#export HADOOP_ROOT=/usr/lib/hadoop
-#export HADOOP_JAR=$HADOOP_ROOT/contrib/streaming/hadoop-streaming-current.jar
-#export HIVE_HOME=/opt/hive/current
-#export PATH=$HIVE_HOME/bin:$PATH

65ec356cbbf407ba95a5eff872099d98362df711
id774 <idnanashi@gmail.com>
2025-01-28 19:12:21 +0900

    172 Improve runcpp function with error handling, dynamic output filenames, and modern C++ standard

--- dot_zsh/plugins/runcpp.zsh
+++ dot_zsh/plugins/runcpp.zsh
@@ -2,4 +2,21 @@
 # Last Change: 28-Jan-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
-function runcpp () { g++ -std=c++0x $1 && shift && ./a.out $@ }
+function runcpp() {
+    if [ -z "$1" ]; then
+        echo "Usage: runcpp <source_file> [args...]"
+        return 1
+    fi
+
+    src="$1"
+    exe="${src%.*}.out"
+
+    g++ -std=c++17 "$src" -o "$exe"
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed."
+        return 2
+    fi
+
+    shift
+    ./"$exe" "$@"
+}

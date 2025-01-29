2d949038714fb92288d196f77847c4bdabb82ced
id774 <idnanashi@gmail.com>
2025-01-30 02:26:24 +0900

    190 Refactor vcs_info zsh to improve consistency and compatibility

--- dot_zsh/plugins/vcs_info.zsh
+++ dot_zsh/plugins/vcs_info.zsh
@@ -1,5 +1,5 @@
 # vcs_info.zsh
-# Last Change: 08-Oct-2014.
+# Last Change: 30-Jan-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 autoload -Uz add-zsh-hook
@@ -7,27 +7,28 @@ autoload -Uz colors
 colors
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git svn hg bzr
+zstyle ':vcs_info:*' enable git svn hg
 zstyle ':vcs_info:*' formats '(%s)-[%b]'
 zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
-zstyle ':vcs_info:(svn|bzr):*' branchformat '%b:r%r'
-zstyle ':vcs_info:bzr:*' use-simple true
+zstyle ':vcs_info:(svn):*' branchformat '%b:r%r'
 
 autoload -Uz is-at-least
 if is-at-least 4.3.10; then
-  zstyle ':vcs_info:git:*' check-for-changes true
-  zstyle ':vcs_info:git:*' stagedstr "+"
-  zstyle ':vcs_info:git:*' unstagedstr "-"
-  zstyle ':vcs_info:git:*' formats '(%s)-[%b] %c%u'
-  zstyle ':vcs_info:git:*' actionformats '(%s)-[%b|%a] %c%u'
+    zstyle ':vcs_info:git:*' check-for-changes true
+    zstyle ':vcs_info:git:*' stagedstr "+"
+    zstyle ':vcs_info:git:*' unstagedstr "-"
+    zstyle ':vcs_info:git:*' formats '(%s)-[%b] %c%u'
+    zstyle ':vcs_info:git:*' actionformats '(%s)-[%b|%a] %c%u'
 fi
 
 function _update_vcs_info_msg() {
-    setopt localoptions NO_shwordsplit unset
+    setopt localoptions NO_shwordsplit
     psvar=()
-    LANG=en_US.UTF-8 vcs_info
+    vcs_info
     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
+
 add-zsh-hook precmd _update_vcs_info_msg
+
 RPROMPT="%1(v|%F{cyan}%1v%f|%F{magenta}[%n]%f)"
 setopt transient_rprompt

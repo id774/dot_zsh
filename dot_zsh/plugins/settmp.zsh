56c651b0aaa658b6b20543eb5e5a52f4d943deab
id774 <idnanashi@gmail.com>
2025-01-30 03:08:00 +0900

    193 Ensure TMP is set only when unset to avoid overriding existing values

--- dot_zsh/plugins/settmp.zsh
+++ dot_zsh/plugins/settmp.zsh
@@ -1,16 +1,14 @@
 # settmp.zsh
-# Last Change: 28-Jan-2025.
+# Last Change: 30-Jan-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 set_tmp_path() {
-    TMP=${1:-/tmp}
-    if [ -d "$1" ]; then
-        TMP="$1"
+    if [ -z "${TMP-}" ]; then
+        TMP="${1}"
+        [ -d "$TMP" ] || TMP="${2}"
+        export TMP TMPDIR="$TMP" TEMPDIR="$TMP"
     fi
-    export TMP
-    export TMPDIR=$TMP
-    export TEMPDIR=$TMP
 }
 
-set_tmp_path "$HOME/.tmp"
+set_tmp_path "$HOME/.tmp" "/tmp"
 unset -f set_tmp_path

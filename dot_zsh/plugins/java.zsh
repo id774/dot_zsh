c7668f0e0a65c192885066a087acd6c5b9341259
id774 <idnanashi@gmail.com>
2025-01-29 23:41:39 +0900

    180 Restrict Java environment setup to non-root users

--- dot_zsh/plugins/java.zsh
+++ dot_zsh/plugins/java.zsh
@@ -14,28 +14,30 @@ set_java_path() {
     done
 }
 
-set_java_path \
-  /usr/java/default \
-  /Library/Java/JavaVirtualMachines/jdk-8.jdk/Contents/Home \
-  /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home \
-  /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home \
-  /Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home \
-  /usr/lib/jvm/java-8-openjdk-i386 \
-  /usr/lib/jvm/java-8-openjdk-amd64 \
-  /usr/lib/jvm/java-8-openjdk \
-  /usr/lib/jvm/java-11-openjdk-i386 \
-  /usr/lib/jvm/java-11-openjdk-amd64 \
-  /usr/lib/jvm/java-11-openjdk \
-  /usr/lib/jvm/java-17-openjdk-i386 \
-  /usr/lib/jvm/java-17-openjdk-amd64 \
-  /usr/lib/jvm/java-17-openjdk \
-  /usr/lib/jvm/java-21-openjdk-i386 \
-  /usr/lib/jvm/java-21-openjdk-amd64 \
-  /usr/lib/jvm/java-21-openjdk \
-  /opt/java/jre \
-  /opt/java/jre/current \
-  /opt/java/jdk \
-  /opt/java/jdk/current
+if [ "$(id -u)" -ne 0 ]; then
+    set_java_path \
+        /usr/java/default \
+        /Library/Java/JavaVirtualMachines/jdk-8.jdk/Contents/Home \
+        /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home \
+        /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home \
+        /Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home \
+        /usr/lib/jvm/java-8-openjdk-i386 \
+        /usr/lib/jvm/java-8-openjdk-amd64 \
+        /usr/lib/jvm/java-8-openjdk \
+        /usr/lib/jvm/java-11-openjdk-i386 \
+        /usr/lib/jvm/java-11-openjdk-amd64 \
+        /usr/lib/jvm/java-11-openjdk \
+        /usr/lib/jvm/java-17-openjdk-i386 \
+        /usr/lib/jvm/java-17-openjdk-amd64 \
+        /usr/lib/jvm/java-17-openjdk \
+        /usr/lib/jvm/java-21-openjdk-i386 \
+        /usr/lib/jvm/java-21-openjdk-amd64 \
+        /usr/lib/jvm/java-21-openjdk \
+        /opt/java/jre \
+        /opt/java/jre/current \
+        /opt/java/jdk \
+        /opt/java/jdk/current
+    export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd"
+fi
 
-export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd"
 unset -f set_java_path

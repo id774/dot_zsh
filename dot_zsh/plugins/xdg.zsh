38058d93d9a97f0fad7fc865d79e91cc8a8a56e7
id774 <idnanashi@gmail.com>
2025-01-30 02:02:31 +0900

    Revert "187 Remove unnecessary XDG path and HISTFILE setting"
    This reverts commit 788b6dd4d01a03d01e34525f612c6c6e2d16e5da.

--- dot_zsh/plugins/xdg.zsh
+++ dot_zsh/plugins/xdg.zsh
@@ -6,6 +6,8 @@ export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 
+export PATH="$XDG_DATA_HOME/bin:$PATH"
+export HISTFILE="$XDG_CACHE_HOME/history"
 export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
 

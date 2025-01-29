f4015b289007a10145975a3d2e3cd0618b693648
id774 <idnanashi@gmail.com>
2025-01-30 02:08:41 +0900

    188 Fix indentation and spacing in pip.zsh

--- dot_zsh/plugins/pip.zsh
+++ dot_zsh/plugins/pip.zsh
@@ -1,13 +1,13 @@
 # pip.zsh
-# Last Change: 01-Sep-2014.
+# Last Change: 30-Jan-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 function _pip_completion {
-  local words cword
-  read -Ac words
-  read -cn cword
-  reply=( $( COMP_WORDS="$words[*]" \
-             COMP_CWORD=$(( cword-1 )) \
-             PIP_AUTO_COMPLETE=1 $words[1] ) )
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $(COMP_WORDS="$words[*]" \
+              COMP_CWORD=$((cword-1)) \
+              PIP_AUTO_COMPLETE=1 $words[1]) )
 }
 compctl -K _pip_completion pip

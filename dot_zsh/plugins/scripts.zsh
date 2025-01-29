c00f646aac8e9ff90c6a1d0ba561e50ba6823562
id774 <idnanashi@gmail.com>
2025-01-30 02:02:42 +0900

    Revert "186 Add XDG base directory support in xdg.zsh"
    This reverts commit 645dcb49508d3834611aab917baf4f02c098f96b.

--- dot_zsh/plugins/scripts.zsh
+++ dot_zsh/plugins/scripts.zsh
@@ -19,6 +19,7 @@ set_scripts_path() {
 if [ "$(id -u)" -ne 0 ]; then
     set_scripts_path "$HOME/scripts"
     set_private_path "$HOME/private/scripts"
+    append_to_path_if_exists "$HOME/.local/bin"
     append_to_path_if_exists "$HOME/bin"
 fi
 

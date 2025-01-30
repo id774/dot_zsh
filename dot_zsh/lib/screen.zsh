2feefe8d64a50ea3cc431b9d8f53a12874602a18
id774 <idnanashi@gmail.com>
2025-01-30 10:08:11 +0900

    196 replace which with command -v for better compatibility

--- dot_zsh/lib/screen.zsh
+++ dot_zsh/lib/screen.zsh
@@ -1,19 +1,21 @@
 # screen.zsh
-# Last Change: 28-Jan-2025.
+# Last Change: 30-Jan-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
 call_exec_screen() {
     if [ "$TERM" != "linux" ] && [ "$TERM" != "xterm-256color" ]; then
-        if [ `ps ax | grep screen | grep -v grep | wc -l` = 0 ]; then
-            which screen > /dev/null 2>&1 && \
+        if [ "$(ps ax | grep screen | grep -v grep | wc -l)" = 0 ]; then
+            if command -v screen > /dev/null 2>&1; then
                 exec screen -U -D -RR
+            fi
         fi
 
         case "${TERM}" in
-          *xterm*|rxvt|(dt|k|E)term)
-            which screen > /dev/null 2>&1 && \
-                exec screen -U -D -RR
-            ;;
+            *xterm*|rxvt|(dt|k|E)term)
+                if command -v screen > /dev/null 2>&1; then
+                    exec screen -U -D -RR
+                fi
+                ;;
         esac
     fi
 }

6d708241d3f12700fad853df667f8e41c7c03271
id774 <idnanashi@gmail.com>
2025-02-01 16:05:19 +0900

    199 Refactor Zsh config loading to use prioritized directory selection

--- dot_zsh/lib/load.zsh
+++ dot_zsh/lib/load.zsh
@@ -1,41 +1,31 @@
 # load.zsh
-# Last Change: 28-Jan-2025.
+# Last Change: 01-Feb-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
 
-call_screen() {
-    if [ -f "$HOME/.zsh/lib/screen.zsh" ]; then
-        . "$HOME/.zsh/lib/screen.zsh"
-    elif [ -f "/usr/local/etc/zsh/lib/screen.zsh" ]; then
-        . "/usr/local/etc/zsh/lib/screen.zsh"
-    elif [ -f "/etc/zsh/lib/screen.zsh" ]; then
-        . "/etc/zsh/lib/screen.zsh"
+ZSH_ROOT=""
+for ZSH_SEARCH_PATH in "$HOME/.zsh" "/usr/local/etc/zsh" "/etc/zsh"; do
+    if [ -d "$ZSH_SEARCH_PATH" ]; then
+        ZSH_ROOT="$ZSH_SEARCH_PATH"
+        break
     fi
+done
+
+[ -z "$ZSH_ROOT" ] && return
+
+call_screen() {
+    [ -f "$ZSH_ROOT/lib/screen.zsh" ] && . "$ZSH_ROOT/lib/screen.zsh"
 }
 
 load_plugins() {
-    if [ -d "$HOME/.zsh/plugins" ]; then
-        for ZSH_PLUGIN in "$HOME/.zsh/plugins"/*.zsh; do
-            [ -f "$ZSH_PLUGIN" ] && . "$ZSH_PLUGIN"
-        done
-    elif [ -d "/usr/local/etc/zsh/plugins" ]; then
-        for ZSH_PLUGIN in "/usr/local/etc/zsh/plugins"/*.zsh; do
-            [ -f "$ZSH_PLUGIN" ] && . "$ZSH_PLUGIN"
-        done
-    elif [ -d "/etc/zsh/plugins" ]; then
-        for ZSH_PLUGIN in "/etc/zsh/plugins"/*.zsh; do
+    if [ -d "$ZSH_ROOT/plugins" ]; then
+        for ZSH_PLUGIN in "$ZSH_ROOT/plugins"/*.zsh; do
             [ -f "$ZSH_PLUGIN" ] && . "$ZSH_PLUGIN"
         done
     fi
 }
 
 load_base() {
-    if [ -f "$HOME/.zsh/lib/base.zsh" ]; then
-        . "$HOME/.zsh/lib/base.zsh"
-    elif [ -f "/usr/local/etc/zsh/lib/base.zsh" ]; then
-        . "/usr/local/etc/zsh/lib/base.zsh"
-    elif [ -f "/etc/zsh/lib/base.zsh" ]; then
-        . "/etc/zsh/lib/base.zsh"
-    fi
+    [ -f "$ZSH_ROOT/lib/base.zsh" ] && . "$ZSH_ROOT/lib/base.zsh"
 }
 
 load_main() {
@@ -46,7 +36,9 @@ load_main() {
 
 load_main "$@"
 
+unset ZSH_ROOT
 unset ZSH_PLUGIN
+unset ZSH_SEARCH_PATH
 unset -f call_screen
 unset -f load_plugins
 unset -f load_base

4d90c7b67ef88e260ba4c72964649cbf24527569
id774 <idnanashi@gmail.com>
2025-02-02 14:03:31 +0900

    204 Add version-based compatibility for pip completion in Zsh

--- dot_zsh/plugins/pip.zsh
+++ dot_zsh/plugins/pip.zsh
@@ -1,13 +1,29 @@
 # pip.zsh
-# Last Change: 30-Jan-2025.
+# Last Change: 02-Feb-2025.
 # Maintainer:  id774 <idnanashi@gmail.com>
+
+autoload -Uz is-at-least
 
 function _pip_completion {
     local words cword
-    read -Ac words
-    read -cn cword
-    reply=( $(COMP_WORDS="$words[*]" \
-              COMP_CWORD=$((cword-1)) \
-              PIP_AUTO_COMPLETE=1 $words[1]) )
+    if is-at-least 5.0; then
+        words=(${(z)${(Q)BUFFER}})
+        cword=$(( ${#words} - 1 ))
+        compadd -- $(COMP_WORDS="${words[*]}" \
+                     COMP_CWORD=$cword \
+                     PIP_AUTO_COMPLETE=1 "${words[1]}")
+    else
+        read -A words
+        read -c cword
+        reply=( $(COMP_WORDS="${words[*]}" \
+                  COMP_CWORD=$((cword-1)) \
+                  PIP_AUTO_COMPLETE=1 ${words[1]}) )
+    fi
 }
-compctl -K _pip_completion pip
+
+if is-at-least 5.0; then
+    compdef _pip_completion pip
+else
+    autoload -Uz compctl
+    compctl -K _pip_completion pip
+fi
